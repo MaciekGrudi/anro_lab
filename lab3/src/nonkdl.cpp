570dca0c1b634cdd6dfbb69ae8d755cd187d34d4
Przemek <przemwil@gmail.com>
2017-04-29 22:21:45 +0200

    zmodyfikowany launch, działa kinematyka prosta nonkdl pozycji

--- lab3/src/nonkdl.cpp
+++ lab3/src/nonkdl.cpp
@@ -2,13 +2,12 @@
 #include <math.h>
 #include "geometry_msgs/PoseStamped.h"
 #include "sensor_msgs/JointState.h"
-#include <tf/transform_datatypes.h>
 
 using namespace std;
 void zbudujMacierz();
 void callback(const sensor_msgs::JointState & msg);
 
-double t1,t2,t3,a2=0.4,d2=0.2;
+double t1,t3,t4,a3=0.4, a4=0.2,d2=0.2;
 double T[4][4];
 
 int main(int argc, char **argv)
@@ -17,36 +16,41 @@ int main(int argc, char **argv)
 
 	ros::NodeHandle n;
 
-	ros::Publisher nonkdl_pub=n.advertise<geometry_msgs::PoseStamped>("posestamped_nonkdl",1000); //zmienic nazwe
+	ros::Publisher nonkdl_pub=n.advertise<geometry_msgs::PoseStamped>("posestamped_nonkdl",1);
 	ros::Subscriber nonkdl_sub=n.subscribe("joint_states",1000,callback);	
 
 	ros::Rate loop_rate(1);
 
-	tf::Quaternion q;
+	//tf::Quaternion q;
 	geometry_msgs::PoseStamped msg;
 	double alpha,beta,gamma;
 	
 	while(ros::ok())
 	{
-		zbudujMacierz();
-	
+		
+        
+        zbudujMacierz();
 		gamma=-atan2(T[0][1],T[1][1]);
 		alpha=0;
 		beta=-M_PI/2.0;
-		q.setRPY(alpha,beta,gamma);
+		//q.setRPY(alpha,beta,gamma);
+        
+
 		msg.header.frame_id="base_link";
 		msg.pose.orientation.x=0;
 		msg.pose.orientation.y=0;
-		msg.pose.orientation.z=1;		
-		msg.pose.orientation.w=0;
+		msg.pose.orientation.z=0;		
+		msg.pose.orientation.w=1;
 
-		msg.pose.position.x=T[3][0];
-		msg.pose.position.y=T[3][1];
-		msg.pose.position.z=T[3][2];
-	
+		msg.pose.position.x=T[0][3];
+		msg.pose.position.y=T[1][3];
+		msg.pose.position.z=T[2][3];
+
+        ROS_DEBUG_STREAM("pos: "<<T[0][3]<<" "<<T[1][3]<<" "<<T[2][3]);
+	    
 		nonkdl_pub.publish(msg);
 	
-		ros::spin();
+		ros::spinOnce();
 	
 		loop_rate.sleep();
 	}
@@ -56,18 +60,18 @@ int main(int argc, char **argv)
 
 void zbudujMacierz()
 {
-	T[0][0]=cos(t1)*cos(t2)*cos(t3) - cos(t1)*sin(t2)*sin(t3);
-	T[0][1]=-cos(t1)*cos(t2)*sin(t3) - cos(t1)*cos(t3)*sin(t2);
+	T[0][0]=cos(t1)*cos(t3)*cos(t4) - cos(t1)*sin(t3)*sin(t4);
+	T[0][1]=-cos(t1)*cos(t3)*sin(t4) - cos(t1)*cos(t4)*sin(t3);
 	T[0][2]=-sin(t1);
-	T[0][3]=a2*cos(t1)*cos(t2) - d2*sin(t1);
-	T[1][0]=cos(t2)*cos(t3)*sin(t1) - sin(t1)*sin(t2)*sin(t3);
-	T[1][1]=-cos(t2)*sin(t1)*sin(t3) - cos(t3)*sin(t1)*sin(t2);
+	T[0][3]=(2*cos(t1)*cos(t3))/5 - (cos(t1)*sin(t3)*sin(t4))/5 + (cos(t1)*cos(t3)*cos(t4))/5;
+	T[1][0]=cos(t3)*cos(t4)*sin(t1) - sin(t1)*sin(t3)*sin(t4);
+	T[1][1]=-cos(t3)*sin(t1)*sin(t4) - cos(t4)*sin(t1)*sin(t3);
 	T[1][2]=cos(t1);
-	T[1][3]=d2*cos(t1) + a2*cos(t2)*sin(t1);
-	T[2][0]=-cos(t2)*sin(t3) - cos(t3)*sin(t2);
-	T[2][1]= sin(t2)*sin(t3) - cos(t2)*cos(t3);
+	T[1][3]=(2*cos(t3)*sin(t1))/5 - (sin(t1)*sin(t3)*sin(t4))/5 + (cos(t3)*cos(t4)*sin(t1))/5;
+	T[2][0]=-cos(t3)*sin(t4) - cos(t4)*sin(t3);
+	T[2][1]=sin(t3)*sin(t4) - cos(t3)*cos(t4);
 	T[2][2]=0;
-	T[2][3]=-a2*sin(t2);
+	T[2][3]=0.2-(cos(t3)*sin(t4))/5 - (cos(t4)*sin(t3))/5 - (2*sin(t3))/5;
 	T[3][0]=0;
 	T[3][1]=0;
 	T[3][2]=0;
@@ -76,8 +80,9 @@ void zbudujMacierz()
 
 void callback(const sensor_msgs::JointState & msg)
 {
-	t1 = msg.position[0];
-	t2=msg.position[1];
-	t3=msg.position[2];
+	t1=msg.position[0];
+	t3=msg.position[1];
+	t4=msg.position[2];
+    ROS_DEBUG_STREAM("joint "<<t1<<" "<<t3<<" "<<t4<<"\n");
 } 
 
